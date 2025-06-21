8b5088db3f284fb5d255bb4f2704ffdb3b14777b
schwollie <lars@familiechristiansen.de>
2025-06-21 13:41:26 +0200

    social media

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    // --- Social Bar Scroll Logic ---
+    const socialBar = document.querySelector('.social-bar');
+    let lastScrollY = window.scrollY;
+
+    window.addEventListener('scroll', () => {
+        const currentScrollY = window.scrollY;
+
+        // If scrolling down past the header, hide the bar
+        if (currentScrollY > lastScrollY && currentScrollY > 100) {
+            socialBar.classList.add('social-bar--hidden');
+        } 
+        // If scrolling up, only show the bar if we are near the top
+        else if (currentScrollY < lastScrollY) {
+            // The bar will only reappear if you scroll up into the top 300px of the page
+            if (currentScrollY < 300) { 
+                 socialBar.classList.remove('social-bar--hidden');
+            }
+        }
+        // Always ensure the bar is visible when at the very top of the page
+        if (currentScrollY < 10) {
+            socialBar.classList.remove('social-bar--hidden');
+        }
+
+        // Update last scroll position for the next event
+        lastScrollY = currentScrollY <= 0 ? 0 : currentScrollY;
+    }, { passive: true });
+
     // --- Configuration ---
     const GALLERY_IMAGE_COUNT = 5; // Set this to the number of gallery images you have
 
